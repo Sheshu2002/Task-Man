5145ec386f9a5b7507aad0afb981bbe1447cea62
Wm Shashank <141142417+Sheshu2002@users.noreply.github.com>
2024-10-21 15:08:15 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://vercel.live/link/task-man-backserver.vercel.app";
 export const getTasks = () => Axios.get(`${API_URL}/tasks`);
 export const createTask = (task) => Axios.post(`${API_URL}/tasks`, task);
 export const updateTask = (id, updatedtask) => Axios.put(`${API_URL}/tasks/${id}`, updatedtask);
