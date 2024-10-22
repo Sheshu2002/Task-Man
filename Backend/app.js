f8971032d7f453accfcbcd55defa67de2e3bae88
Sheshu2002 <wmshashank123@gmail.com>
2024-10-22 11:23:43 +0530

    removed

--- Backend/app.js
+++ Backend/app.js
@@ -19,21 +19,17 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://task-man-chotu.vercel.app'); // Replace with your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://task-man-chotu.vercel.app'); // Replace with your frontend domain
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
+//   next();
+// });
 
 app.use(taskroutes);
 
 
 
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src *; script-src *; frame-src *");
-  next();
-});
 
 
 
